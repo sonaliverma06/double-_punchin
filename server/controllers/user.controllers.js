253e6711d7da4483b8ff63a29d4e3ef8bfe789a7
sonaliverma06 <sonali.badoliya@iflair.com>
2023-05-08 17:52:56 +0530

    work

---
+++ server/controllers/user.controllers.js
@@ -0,0 +1,568 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+const router = express.Router();
+const saltRounds = 10;
+const jwt = require("jsonwebtoken");
+const db = require("../database");
+const pool = db();
+// const cron = require('node-cron');
+const schedule = require("node-schedule")
+// router.put("/user/:id", async (req, res) => {
+//   const ids = req.params.id;
+//   const { username, contact, address, city } = req.body;
+//   console.log('req.body',req.body,ids)
+
+//   await pool.getConnection((err, connection) => {
+//     if (err) {
+//       return res.status(500).json({
+//         message: "Fail to connect",
+//       });
+//     }
+//     connection.query(
+//       "UPDATE user SET username=?, contact=? , address=?, city=? WHERE id = ?",
+//       [username, contact, address, city, ids],
+//       (error, response) => {
+//         if (error) {
+//           return res.status(500).json({
+//             message: "error in fetching data",
+//           });
+//         } else {
+//           return res.status(201).json({
+//             message: "success",
+//             data: response,
+//           });
+//         }
+//       }
+//     );
+//   });
+// });
+
+// router.delete("/user/:id", async (req, res) => {
+//   const ids = req.params.id;
+//   await pool.getConnection((err, connection) => {
+//     if (err) {
+//       return res.status(500).json({
+//         message: "Fail to connect",
+//       });
+//     }
+//     connection.query(
+//       "DELETE FROM user WHERE id = ?",
+//       [ids],
+//       (error, response) => {
+//         if (error) {
+//           return res.status(500).json({
+//             message: "error in fetching data",
+//           });
+//         } else {
+//           return res.status(201).json({
+//             message: "success",
+//             data: response,
+//           });
+//         }
+//       }
+//     );
+//   });
+// });
+
+router.post("/signup", async (req, res) => {
+  const data = req.body.user;
+ const { Name, email, password } = data;
+ if (
+    !data ||
+    !data.Name ||
+    !data.email ||
+    !data.password ||
+    !data.repassword
+  ) {
+    console.log(data.Name, data.email, data.password, data.repassword);
+    return res.status(500).send({ message: "Not provided required details" });
+  }
+
+  await pool.getConnection((err, connection) => {
+    if (err) return res.status(500).json(err);
+    if (res.length) return res.status(409).json("User already exists!");
+
+    bcrypt.hash(password, saltRounds).then((hash) => {
+      connection.query(
+        "INSERT INTO signup(name, email, password,role) VALUES(?,?,?,?)",
+        [Name, email, hash,"user"],
+        (error, response) => {
+          connection.release();
+          console.log("error, responsesignup", error, response);
+          if (error) {
+            return res.status(500).json({
+              message: "error in posting data",
+            });
+          } else {
+            return res.status(201).json({
+              message: "success",
+              data: response,
+            });
+          }
+        }
+      );
+    });
+  });
+});
+
+router.post("/login", async (req, res) => {
+  // console.log("req", req.body);
+  const { email } = req.body.log;
+  await pool.getConnection((err, connection) => {
+    if (err) {
+      return res.status(500).json({
+        message: "Failed to connect",
+      });
+    }
+    connection.query(
+      "SELECT * FROM signup WHERE email = ?",
+      [email],
+      (error, response) => {
+        connection.release();
+      if (error) {
+          return res.status(500).json({
+            message: "error in posting data",
+          });
+        } else {
+          if (response.length === 0) {
+            return res.status(404).json({
+              message: "User not found!",
+            });
+          } else {
+     
+            const token = jwt.sign(
+              { id: response[0].id, email: response[0].email },
+              "jwtkey"
+            );
+            console.log("token", token);
+            return res.status(201).json({
+              message: "success",
+              data: response,
+              token: token,
+            });
+          }
+        }
+      }
+    );
+  });
+});
+
+ 
+router.post("/assishift/:id", async (req, res) => {
+    // const id = req.params.id;
+    // console.log("ID", id);
+console.log("body12345", req.body);
+  const data = req.body.assig;
+  const {shiftid,user} = data;
+  console.log("data", data);
+
+  await pool.getConnection((err, connection) => {
+    if (err) {
+      return res.status(500).json({
+        message: "Failed to connect",
+      });
+    }
+   
+    connection.query(
+      "INSERT INTO assingshift(shiftid,user) VALUES(?,?)",
+      [shiftid,user],
+      (error, response) => {
+         console.log("error", "response", error, response);
+       connection.release();
+        if (error) {
+          return res.status(500).json({
+            message: "error in posting data",
+          });
+        } else {
+          return res.status(201).json({
+            message: "success",
+            data: response,
+          });
+        }
+      }
+    );
+  });
+});
+
+
+router.get("/assishiftuser", async (req, res) => {
+  await pool.getConnection((err, connection) => {
+    if (err) {
+      return res.status(500).json({
+        message: "Fail to connect",
+      });
+    }
+    // connection.query("SELECT * FROM assingshift  ", (error, response) => {
+    connection.query(
+      "SELECT * FROM assingshift INNER JOIN shift ON `shift`.`id`= `assingshift`.`shiftid`",
+      (error, response) => {
+       connection.release();
+        if (error) {
+          return res.status(500).json({
+            message: "error in fetching data",
+          });
+        } else {
+          return res.status(201).json({
+            message: "success",
+            data: response,
+          });
+        }
+      }
+    );
+  });
+});
+
+
+router.get("/assishiftuser/:id", async (req, res) => {
+  await pool.getConnection((err, connection) => {
+    if (err) {
+      return res.status(500).json({
+        message: "Fail to connect",
+      });
+    }
+    // connection.query("SELECT * FROM assingshift  ", (error, response) => {
+    connection.query(
+      "SELECT `shift`.* FROM `shift` JOIN `assingshift` ON `assingshift`.`shiftid` = `shift`.`id` WHERE `assingshift`.`user` = ?",[req.params.id],
+      (error, response) => {
+       connection.release();
+        if (error) {
+          return res.status(500).json({
+            message: "error in fetching data",
+          });
+        } else {
+          return res.status(201).json({
+            message: "success",
+            data: response,
+          });
+        }
+      }
+    );
+  });
+});
+
+
+router.get("/shiftuser", async (req, res) => {
+  await pool.getConnection((err, connection) => {
+    if (err) {
+      return res.status(500).json({
+        message: "Fail to connect",
+      });
+    }
+    connection.query("SELECT * FROM signup", (error, response) => {
+    connection.release();
+      if (error) {
+        return res.status(500).json({
+          message: "error in fetching data",
+        });
+      } else {
+        return res.status(201).json({
+          message: "success",
+          data: response,
+        });
+      }
+    });
+  });
+});
+
+
+router.get("/shiftuser/:id", async (req, res) => {
+  const ids = req.params.id;
+ await pool.getConnection((err, connection) => {
+    if (err) {
+      return res.status(500).json({
+        message: "Fail to connect",
+      });
+    }
+    connection.query(
+      "SELECT * FROM signup WHERE id = ?",
+         [ids],
+      (error, response) => {
+        connection.release();
+       
+        if (error) {
+          return res.status(500).json({
+            message: "error in fetching data",
+          });
+        } else {
+          return res.status(201).json({
+            message: "success",
+            data: response,
+          });
+        }
+      }
+    );
+  });
+});
+
+
+router.get("/getshift", async (req, res) => {
+  await pool.getConnection((err, connection) => {
+    if (err) {
+      return res.status(500).json({
+        message: "Fail to connect",
+      });
+    }
+    connection.query("SELECT * FROM shift", (error, response) => {
+      connection.release();
+      if (error) {
+        return res.status(500).json({
+          message: "error in fetching data",
+        });
+      } else {
+        return res.status(201).json({
+          message: "success",
+          data: response,
+        });
+      }
+    });
+  });
+});
+
+
+
+
+router.get("/user", async (req, res) => {
+  await pool.getConnection((err, connection) => {
+    if (err) {
+      return res.status(500).json({
+        message: "Fail to connect",
+      });
+    }
+    connection.query("SELECT * FROM attendance", (error, response) => {
+      connection.release();
+      if (error) {
+        return res.status(500).json({
+          message: "error in fetching data",
+        });
+      } else {
+        return res.status(201).json({
+          message: "success",
+          data: response,
+        });
+      }
+    });
+  });
+});
+
+router.get("/user/:id", async (req, res) => {
+  const ids = req.params.id;
+  // console.log("ids", ids);
+  await pool.getConnection((err, connection) => {
+    if (err) {
+      return res.status(500).json({
+        message: "Fail to connect",
+      });
+    }
+  
+    connection.query(
+      `SELECT date, GROUP_CONCAT(punchin) as 'attendance' FROM attendance WHERE user_id=${ids} group by date`,
+      
+  
+      [ids],
+      (error, response) => {
+        connection.release();
+        if (error) {
+          return res.status(500).json({
+            message: "error in fetching data",
+          });
+        } else {
+          return res.status(201).json({
+            message: "success",
+            data: response,
+          });
+        }
+      }
+    );
+  
+  });
+});
+
+router.post("/punchatten/:id", async (req, res) => {
+  // const id = req.body.id;
+  // console.log("body999999999", req.body.id);
+
+  const user_id = req.params.id;
+  const data = req.body;
+ const id =  req.params.id;
+ console.log("gfgggggggggg id",id);
+
+  console.log("data111111",data);
+  const { punchin, date } = data;
+  await pool.getConnection((err, connection) => {
+    if (err) {
+      return res.status(500).json({
+        message: "Failed to connect",
+      });
+    }
+    const finddata = `SELECT * FROM attendance WHERE user_id=${user_id}`;
+    connection.query(finddata, (error, response) => {
+      console.log("error","response",error,response);
+      if (error) {
+        return res.status(500).json({
+          message: "error in fetching data",
+        });
+      } else {
+        connection.query(
+          `UPDATE attendance SET punchin=?, user_id="${user_id}" WHERE id = "3"`,
+          //       [username, contact, address, city, ids],
+          // "INSERT INTO attendance(punchin, date, user_id) VALUES(?,?,?)",
+          [JSON.stringify(punchin), user_id],
+          (error, response) => {
+            console.log("error","response5555555",error,response);
+            connection.release();
+
+            if (error) {
+              return res.status(500).json({
+                message: "error in posting data",
+              });
+            } else {
+              return res.status(201).json({
+                message: "success",
+                data: response,
+              });
+            }
+          }
+        );
+      }
+    });
+  });
+});
+
+router.post("/addproduct", async (req, res) => {
+  const data = req.body.pro;
+  const { category, price, size, uploadedfile, quantity } = data;
+await pool.getConnection((err, connection) => {
+    if (err) {
+      return res.status(500).json({
+        message: "Failed to connect",
+      });
+    }
+    connection.query(
+      "INSERT INTO product(category,price,size,uploadedfile,quantity) VALUES(?,?,?,?,?)",
+      [category, price, size, uploadedfile, quantity],
+      (error, response) => {
+        connection.release();
+        if (error) {
+          return res.status(500).json({
+            message: "error in posting data",
+          });
+        } else {
+          return res.status(201).json({
+            message: "success",
+            data: response,
+          });
+        }
+      }
+    );
+  });
+});
+
+router.get("/product", async (req, res) => {
+  await pool.getConnection((err, connection) => {
+    if (err) {
+      return res.status(500).json({
+        message: "Fail to connect",
+      });
+    }
+    connection.query("SELECT * FROM product", (error, response) => {
+      connection.release();
+      if (error) {
+        return res.status(500).json({
+          message: "error in fetching data",
+        });
+      } else {
+        return res.status(201).json({
+          message: "success",
+          data: response,
+        });
+      }
+    });
+  });
+});
+
+router.get("/product/:id", async (req, res) => {
+  const ids = req.params.id;
+  console.log("ids", ids);
+  await pool.getConnection((err, connection) => {
+    if (err) {
+      return res.status(500).json({
+        message: "Fail to connect",
+      });
+    }
+    connection.query(
+      "SELECT * FROM product WHERE id = ?",
+      [ids],
+      (error, response) => {
+        connection.release();
+
+        if (error) {
+          return res.status(500).json({
+            message: "error in fetching data",
+          });
+        } else {
+          return res.status(201).json({
+            message: "success",
+            data: response,
+          });
+        }
+      }
+    );
+  });
+});
+
+// schedule.scheduleJob("*/5 * * * * *", async function() {
+//   await pool.getConnection((err, connection) => {
+//     if (err) {
+//       console.log("err",err)
+//     }
+//     connection.query("SELECT * FROM signup", (error, response) => {
+//       connection.release();
+//       if (error) {
+//        console.log("error",error);
+//       } else {
+//         console.log("respoonse", response);
+//         response.map((i) => {
+//           console.log("i",i.id);
+//           connection.query(
+//             "INSERT INTO attendance(punchin, date, user_id) VALUES(?,?,?)",["[]",new Date().toLocaleDateString('en-US', {year: 'numeric', month: '2-digit', day: '2-digit'}),i.id],(error1,response1)=>{
+//               if(error1 ){
+//                 console.log("error1",error1);
+//               }
+//               else if(response1){
+//                 console.log("response1",response1)
+//               }
+//               else{
+//                 console.log("do nothing");
+//               }
+//             }
+//           )
+//         })
+//       }
+//     });
+//   });
+// });
+
+
+router.get("/holidays", async (req, res) => {
+  await pool.getConnection((err, connection) => {
+    if (err) {
+      return res.status(500).json({
+        message: "Fail to connect",
+      });
+    }
+    connection.query("SELECT * FROM holiday", (error, response) => {
+      connection.release();
+      if (error) {
+        return res.status(500).json({
+          message: "error in fetching data",
+        });
+      } else {
+        return res.status(201).json({
+          message: "success",
+          data: response,
+        });
+      }
+    });
+  });
+});
+module.exports = router;
---
+++ src/Components/Attendance.js
@@ -0,0 +1,199 @@
+import axios from "axios";
+import moment from "moment";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { Modal } from "react-bootstrap";
+import Table from "react-bootstrap/Table";
+import { Link } from "react-router-dom";
+
+
+export default function Attendance() {
+  let use = localStorage.getItem("user");
+  const handleClose = () => setShow(false);
+  const [show, setShow] = useState(false);
+  const [punchtime, setpunchtime] = useState([]);
+  const [getuser1, setgetuser1] = useState([]);
+  const [holi,setholi] =useState([]);
+  // console.log("holi",holi);
+
+  const getpunchindata = () => {
+    axios.get(`http://localhost:4800/user/${use}`).then(function (response) {
+      // console.log("response", response);
+      setpunchtime(response.data.data);
+    });
+  };
+
+  useEffect(() => {
+    getpunchindata();
+  }, []);
+
+const getholidayData = ()=>{
+  axios.get("http://localhost:4800/holidays").then(function (response) {
+    // console.log("response", response);
+    // const a = moment(new Date()).format('YYYY-MM-DD')
+  
+    // const b = response.data.data.filter((item)=>item.date === a )
+    // return(
+    //   setholi(b)
+    // )
+    // console.log("item",item);
+    setholi(response.data.data)
+
+  });
+};
+
+  useEffect (()=>{
+    getholidayData()
+
+  },[])
+
+  const handleShow = (item) => {
+    // console.log(item);
+    setShow(true);
+    setgetuser1(
+      item?.attendance
+        ?.replaceAll(/[/[","]|]/g, " ")
+        .trim()
+        .split("   ")
+    );
+  };
+  // let date = moment(new Date()).format('YYYY-MM-DD')
+  // console.log("date",date);
+
+  return (
+    <div>
+      <Table striped bordered hover variant="dark">
+        <thead>
+          <tr>
+            <th>date </th>
+            <th>Effective Hours</th>
+            {/* <th>Gross Hours</th> */}
+            <th>Total Break</th>
+            <th>Log</th>
+
+            {/* <th>holiday</th> */}
+          </tr>
+        </thead>
+        <tbody>
+         {punchtime &&
+            punchtime.map((item) => {
+              console.log("item",item);
+              let item1 = item?.attendance
+                .replaceAll(/[/[","]|]/g, " ")
+                .trim()
+                .split("   ");
+
+              var totalbreakStartTime = moment(
+                item1[1] ? item1[1] : item1[0],
+                "hh:mm:ss"
+              );
+
+              var totalbreakEndTime = moment(
+                item1[4]
+                  ? item1[4]
+                  : item1[2]
+                  ? item1[2]
+                  : item1[1]
+                  ? item1[1]
+                  : item1[0],
+                "hh:mm:ss"
+              );
+
+              var effectivehrs = moment
+                .utc(totalbreakEndTime.diff(totalbreakStartTime))
+                .format("HH");
+              var effectivemin = moment
+                .utc(totalbreakEndTime.diff(totalbreakStartTime))
+                .format("mm");
+              var effectivesec = moment
+                .utc(totalbreakEndTime.diff(totalbreakStartTime))
+                .format("ss");
+
+              const totalBreak = [
+                effectivehrs,
+                effectivemin,
+                effectivesec,
+              ].join(":");
+              // console.log("brak", totalBreak);
+              var grossStartTime = moment(item1[0], "hh:mm:ss");
+              var grossEndTime = moment(
+                item1[5]
+                  ? item1[5]
+                  : item1[3]
+                  ? item1[3]
+                  : item1[1]
+                  ? item1[1]
+                  : item1[0],
+                "hh:mm:ss"
+              );
+
+              var grosshrs = moment
+                .utc(grossEndTime.diff(grossStartTime))
+                .format("HH");
+              var grossmin = moment
+                .utc(grossEndTime.diff(grossStartTime))
+                .format("mm");
+              var grosssec = moment
+                .utc(grossEndTime.diff(grossStartTime))
+                .format("ss");
+              const grossHours = [grosshrs, grossmin, grosssec].join(":");
+
+              var time1 = moment(totalBreak, "HH:mm:ss");
+
+              var time2 = moment(grossHours, "HH:mm:ss");
+
+              const effectiveHours = moment
+                .utc(moment(time2, "HH:mm:ss").diff(moment(time1, "HH:mm:ss")))
+                .format("HH:mm:ss");
+
+              return (
+                <tr>
+                  {}
+                 <td>{item.date }</td>
+                 
+                 {/* <td>{effectiveHours}</td> */}
+                  <td>{grossHours}</td>
+                  <td>{totalBreak}</td>
+                  <td>
+                    <button
+                      className="bg bg-info"
+                      onClick={() => {
+                        handleShow(item);
+                      }}
+                    >
+                      View Details
+                    </button>
+                  </td>
+                  {/* <td> 
+                  {holi.map((e) => e.date).find((e) => e === moment(new Date()).format('YYYY-MM-DD'))}
+                 </td> */}
+                </tr>
+              );
+            })}
+             
+        </tbody>
+        <Modal show={show} onHide={() => handleClose()} animation={false}>
+          <Modal.Body>
+            <div className="mbody"> 
+            {getuser1 &&
+              getuser1.map((item, index) => (
+                <div className="tab">
+                 <div className="index">{index % 2 == 0 ? "Punch In -": "Punch Out -"}</div>
+                  <div className="item">{item}</div>
+                </div>
+              ))}
+              </div>
+          </Modal.Body>
+          <Modal.Footer>
+            <button variant="secondary" onClick={handleClose}>
+              Close
+            </button>
+          </Modal.Footer>
+        </Modal>
+      </Table>
+     
+      <Link to={"/dat"}>Attendance</Link>;
+  
+    </div>
+  );
+}
